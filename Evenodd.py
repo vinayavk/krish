d8907cf894f4e8d6a44c0eeb82dcb42215a6cfee
vamshikrishna00 <40817293+vamshikrishna00@users.noreply.github.com>
2018-07-05 11:33:46 +0530

    Update Evenodd.py

--- Evenodd.py
+++ Evenodd.py
@@ -1,7 +1,13 @@
-a= int(input())
-if (a%2==0)
-      print("even")
+a=int(raw_input())
+if a%2==0:
+     print("a is Even")
+elif a==0:
+     print("a  is zero")
+else:
+     print("a is odd")
 
-else
-      print("odd")
+
+
+
+
       
