755865137cd683fded4cea4842f51ac1b7e63963
vamshikrishna00 <40817293+vamshikrishna00@users.noreply.github.com>
2018-07-10 11:48:41 +0530

    Update printntimes.c

--- printntimes.c
+++ printntimes.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	char c[50];
+	char c[]="Hello";
 	int n,i;
 	scanf("%d",&n);
-	scanf("%s",c);
+	//scanf("%s",c);
 	for(i=0;i<n;i++)
 	{
-		printf("%s\n",c);	
+		printf("%s\n",c);
+		
+		
 	}
-	}
+	
+	
+	
+}
