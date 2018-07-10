ae162dffea2b86d745aa2eb483f092ff644bc7f1
vamshikrishna00 <40817293+vamshikrishna00@users.noreply.github.com>
2018-07-10 13:06:30 +0530

    Update commchar.py

--- commchar.py
+++ commchar.py
@@ -1,6 +1,14 @@
-a=input("Enter first string:")
-b=input("Enter second string:")
-c=list(set(a)&set(b))
-print("The common letters are:")
-for i in c:
-    print(i)
+i=int(input())
+l=[]
+for x in range(i):
+    j=input("")
+    l.append(j)
+b=[]
+for i in range(len(l)):
+    b.append(list(l[i]))
+for k in range(1):
+    for s in range(len(b[k])):
+        if(b[k][s]==b[k+1][s]):
+            print(b[k][s],end="")
+        else:
+            break
