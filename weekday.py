b871f292eac37365e4b2cad5abf59e057c2929a4
vamshikrishna00 <40817293+vamshikrishna00@users.noreply.github.com>
2018-07-12 12:50:31 +0530

    Update weekday.py

--- weekday.py
+++ weekday.py
@@ -1,5 +1,5 @@
-a=['Monday','Tuesday','Thursday','Friday','Saturday']
-b=['Sunday','Wednesday']
+a=['Monday','Tuesday','Thursday','Friday','Wednesday']
+b=['Sunday','Saturday']
 c=str(input())
 if c in a:
     print('no')
